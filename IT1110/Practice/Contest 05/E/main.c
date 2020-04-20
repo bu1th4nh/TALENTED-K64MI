51384a7e8ac5eae742f16b6cafbc732831fef4c9
bu1th4nh <thanh10973@gmail.com>
2020-04-20 14:52:56 +0700

    Restructure and addition

---
+++ IT1110/Practice/Contest 05/E/main.c
@@ -0,0 +1,215 @@
+/*==========================================================================================*\
+**                        _           _ _   _     _  _         _                            **
+**                       | |__  _   _/ | |_| |__ | || |  _ __ | |__                         **
+**                       | '_ \| | | | | __| '_ \| || |_| '_ \| '_ \                        **
+**                       | |_) | |_| | | |_| | | |__   _| | | | | | |                       **
+**                       |_.__/ \__,_|_|\__|_| |_|  |_| |_| |_|_| |_|                       **
+\*==========================================================================================*/
+//=====================================
+//Solution Briefing - Foreword
+
+
+
+//=====================================
+//Libraries and namespaces
+//#include <bits/stdc++.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+#include<assert.h>
+#include<math.h>
+#include<stdbool.h>
+
+//#define DEBUG
+#define OPTIONAL_FEATURE
+
+//=====================================
+//Macroes
+#define sp ' '
+#define el '\n'
+#define task ""
+#define maxinp ()
+#define fi first
+#define se second
+#define pb push_back
+#define whole(x) x.begin(),x.end()
+#define whole_1(x) x.begin()+1,x.end()
+#define r_whole(x) x.rbegin(),x.rend()
+#define FOR(i, x, y) for(auto i=x; i<=y; ++i)
+#define FORl(i, x, y) for(auto i=x; i<y; ++i)
+#define FORb(i, x, y) for(auto i=x; i>=y; --i)
+#define FORlb(i, x, y) for(auto i=x; i>y; --i)
+#define MEMS(x, val) memset(x, val, sizeof(x))
+#define FILEOP()                        \
+{                                       \
+    freopen(task".inp", "r", stdin);    \
+    freopen(task".out", "w", stdout);   \
+}
+#define FILEOP_DEBUG()                  \
+{                                       \
+    freopen(task".inp", "r", stdin);    \
+    freopen(task".out", "w", stdout);   \
+    freopen(task".err", "w", stderr);   \
+}
+
+//Macroes - Optional
+#ifdef OPTIONAL_FEATURE
+    #define pc(x) putchar(x)
+    #define gc() getchar()
+#endif
+
+
+
+//=====================================
+//Auxilary Functions and Fast I/O
+#ifdef __cplusplus
+    template<class T, class R> T max(const T &__X, const R &__Y)
+    {
+        return __X > __Y ? __X : __Y;
+    }
+    template<class T, class R> T min(const T &__X, const R &__Y)
+    {
+        return __X < __Y ? __X : __Y;
+    }
+    template<class T, class R> void maximize(T &__X, R __Y)
+    {
+        __X = __X > __Y ? __X : __Y;
+    }
+    template<class T, class R> void minimize(T &__X, R __Y)
+    {
+        __X = __X < __Y ? __X : __Y;
+    }
+    template<class T> int getBit(T &__X, int __i)
+    {
+        return ((__X >> __i) & 1) == 1;
+    }
+    template<class T> bool inRange(T __L, T __R, T __X)
+    {
+        return __L <= __X && __X <= __R;
+    }
+    template<class T> T __abs(T __X)
+    {
+        return (__X < 0) ? -__X : __X;
+    }
+    template<class T> T __sqr(T __X)
+    {
+        return __X * __X;
+    }
+
+
+//Fast I/O
+template<class T> inline void scan(T &__ret)
+{
+    __ret = T();
+    char c = 0;
+    bool neg = 0;
+
+    while(isdigit(c) == 0 && c != '-') c = getchar();
+    if(c == '-')
+    {
+        neg = 1;
+        c = getchar();
+    }
+
+    for(; isdigit(c) != 0; c = getchar()) __ret = __ret * 10 + c - '0';
+    __ret = (neg) ? -__ret : __ret;
+}
+template<class T> void print(T __X)
+{
+    if(__X < 0)
+    {
+        putchar('-');
+        __X *= -1;
+    }
+
+    if(__X > 9) print(__X / 10);
+    putchar(__X % 10 + '0');
+}
+#endif
+//=====================================
+//Constants
+
+
+
+//=====================================
+//Typedefs
+typedef long long ll;
+typedef unsigned long long ull;
+char str[128][1000], fix[128][1000];
+int n;
+
+
+//=====================================
+//Functions and procedures
+//File I/O and utilities
+void FileInit()
+{
+    FILEOP()
+}
+void FileDebug()
+{
+    #ifdef DEBUG
+        FILEOP_DEBUG()
+    #else
+        FILEOP()
+    #endif
+}
+void FileClose()
+{
+    fclose(stdin);
+    fclose(stdout);
+}
+
+
+//Enter
+void Enter()
+{
+    scanf("%d\n", &n);
+	FOR(i, 1, n) gets(str[i]);
+}
+
+//Process
+void Formalize(int nth)
+{
+    int cnt = 0, start_pos = 0, sz = strlen(str[nth]);
+    bool pre_space = 1;
+
+    while(str[nth][start_pos] == ' ' && start_pos < sz) ++start_pos;
+    while(str[nth][sz-1] == ' ' && sz > 0) --sz;
+
+    FORl(i, start_pos, sz)
+    {
+        if(str[nth][i] == ' ')
+        {
+            if(pre_space) continue;
+            else fix[nth][cnt++] = ' ', pre_space = 1;
+        }
+        else
+        {
+            fix[nth][cnt++] = pre_space ? toupper(str[nth][i]) : tolower(str[nth][i]);
+            if(pre_space) pre_space = 0;
+        }
+    }
+}
+
+
+//Output
+void Solve()
+{
+    FOR(i, 1, n) Formalize(i);
+    qsort(fix+1, n, 1000, strcmp);
+    FOR(i, 1, n) printf("(%d)%s\n", i, fix[i]);
+}
+
+//Main Procedure
+int main(int argc, char *argv[])
+{
+    Enter();
+    Solve();
+	return 0;
+}
+
+//=============================================================================//
+/**    CTB, you are always in my heart and in my code <3       #30yearsCTB    **/
+//=============================================================================//
