01719d4ca93f3f0c9aa36e73e68f60a356572490
bu1th4nh <thanh10973@gmail.com>
2020-10-31 22:21:35 +0700

    Jacobi method

--- MI3040/report-code/newton.py
+++ MI3040/report-code/newton.py
@@ -9,46 +9,20 @@ import sys
 # Phần thuật toán chính
 class newton_mat_inversion:
 #{
-    def __init__(self, A, n, eps):
+    max_attempt = 2;
+
+    def __init__(self, A, n, eps):    # Khởi tạo
     #{
-        A = np.array(A);
-        self.A = np.reshape(A, (n, n));
+        self.A = np.reshape(np.array(A), (n, n));
         self.n = n;
         self.eps = eps;
+        self.nr_iterations = 0;
     #}
-    def norm(self, __A, __norm_type = 2):
+    def norm(self, __A, __norm_type = 2):   # Chuẩn ma trận
     #{
         return np.linalg.norm(__A, __norm_type);
     #}
-
-
-    # PP Newton nguyên bản
-    def pure_newton(self, X_0):
-    #{
-        # Gán các biến cơ bản
-        norm_X0 = self.norm(X_0); # Do X_0 không đổi nên ta đặt 1 biến làm chuẩn của X_0
-        E       = np.eye(self.n);
-        A       = self.A;
-        eps     = self.eps;
-
-        # Bước 2 của thuật toán
-        q = self.norm(E - A @ X_0);
-        X = X_0;
-        k = 0;
-
-        # Lặp
-        while(norm_X0 * (q ** (1 << k)) >= self.eps * (1 - q)):
-        #{
-            X = X @ (2 * E - A @ X);
-            k = k + 1;
-        #}
-
-        # Đưa ra ma trận cuối cùng
-        return X;
-    #}
-
-    # Tìm xấp xỉ đầu:
-    def refine_initial_approx(self):
+    def __refine_initial_approx(self):  # Tìm xấp xỉ đầu:
     #{
         # Gán các biến cơ bản
         E  = np.eye(self.n);
@@ -62,31 +36,89 @@ class newton_mat_inversion:
 
         # Hiệu chỉnh lại giá trị q của ma trận xấp xỉ đầu
         attempt = 0;
-        max_attempt = 100;
-        while(attempt <= max_attempt):
+        while(attempt <= newton_mat_inversion.max_attempt):
         #{
             X = X @ (2 * E - A @ X);
-            if(self.norm(E - A @ X) < 1): attempt = attempt + 1;
+            if(self.norm(E - A @ X) < 1): attempt += 1;
+            self.nr_iterations += 1 * bool(attempt <= newton_mat_inversion.max_attempt);
         #}
 
         # Trả về ma trận xấp xỉ đầu
         return X;
     #}
-
-
-    def improved_newton(self):
+    def __pure_newton(self, X_0):  # Lặp Newton nguyên bản
     #{
-        return self.pure_newton(self.refine_initial_approx());
+
+        # Gán các biến cơ bản
+        norm_X0 = self.norm(X_0); # Do X_0 không đổi nên ta đặt 1 biến làm chuẩn của X_0
+        E       = np.eye(self.n);
+        A       = self.A;
+        eps     = self.eps;
+        
+
+        # Bước 2 của thuật toán
+        q2k = q = self.norm(E - A @ X_0);
+        X = X_0;
+
+        # Kiểm tra điều kiện hội tụ
+        if(q >= 1):
+        #{
+            print("Xấp xỉ đầu không thỏa mãn nên không đưa ra được ma trận nghịch đảo.");
+            return np.full((self.n, self.n), float("NaN"));
+        #}
+
+
+        # Lặp
+        while(norm_X0 * q2k >= self.eps * (1 - q)):
+        #{
+            self.nr_iterations += 1;
+            X = X @ (2 * E - A @ X);
+            q2k = q2k ** 2
+        #}
+
+        # Đưa ra ma trận cuối cùng
+        print(f"Newton iteration method ended after {self.nr_iterations} iterations", file=sys.stderr);
+        return X;
+    #}
+
+
+    def improved_newton(self):  # PP Newton cải tiến với xấp xỉ đầu
+    #{
+        if(np.linalg.det(self.A) == 0):
+        #{
+            print("A không khả nghịch nên không đưa ra được ma trận nghịch đảo");
+            return np.full((self.n, self.n), float("NaN"));
+        #}
+        return self.__pure_newton(self.__refine_initial_approx());
+    #}
+    def pure_newton(self, X_0):  # PP Newton nguyên bản
+    #{
+        if(np.linalg.det(self.A) == 0):
+        #{
+            print("A không khả nghịch nên không đưa ra được ma trận nghịch đảo");
+            return np.full((self.n, self.n), float("NaN"));
+        #}
+        return self.__pure_newton(X_0);
     #}
 #}
 
 
 #===================================================================================
 # Chương trình ví dụ
-A = np.random.rand(3,3);
-A = np.array([1, 5, 2, 7, 6, 4, 0, 14, 2]);
-A = np.reshape(A, (3, 3));
-uu = newton_mat_inversion(A, 3, 1e-10);
+# A = np.random.rand(3,3);
+# A = np.array([1, 3, 2, 7, 1, 9, 2, 5, 4]);
+# A = np.reshape(A, (3, 3));
+# uu = newton_mat_inversion(A, 3, 1e-10);
+# A1 = uu.improved_newton();
+# print(A1);
+# print(A1 @ A);
 
-print(uu.improved_newton());
+# B = np.array([4, 0.24, -0.08, 0.09, 3, -0.15, 0.04, -0.08, 4]);
+# B = np.reshape(B, (3, 3));
+# vv = newton_mat_inversion(B, 3, 1e-10);
+# B1 = vv.improved_newton();
+# print(B1);
+# print(B1 @ B);
 
+# print(uu.A);
+# print(vv.A);
